04bbb00ab71a60764fc83a4fe4e1f1d26fa6912d
ArsenYavorskyi <ars.yar24@gmail.com>
2023-03-20 11:50:12 +0200

    added first approch

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
 import { print } from './js/lib.js';
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
+   Code repository: https://github.com/yazdrahobycha/a-tiny-JS-world
    Web app: _put project's github pages URL here_
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const dog = {
+    species: 'dog',
+    name: 'Sharik',
+    gender: 'male',
+    hands: 0,
+    legs: 4,
+    saying: 'GAV!!!',
+    friends: ['Vova'],
+};
 
+const cat = {
+    species: 'cat',
+    name: 'Dina',
+    gender: 'female',
+    hands: 0,
+    legs: 4,
+    saying: 'Miau!',
+    friends: ['Sasha', 'Liza'],
+};
 
+const woman = {
+    species: 'human',
+    name: 'Sasha',
+    gender: 'female',
+    hands: 2,
+    legs: 2,
+    saying: 'Iu!',
+    friends: ['Dina', 'Liza'],
+};
+
+const man = {
+    species: 'human',
+    name: 'Vova',
+    gender: 'male',
+    hands: 2,
+    legs: 2,
+    saying: 'Based!',
+    friends: ['Sharik', 'Liza'],
+};
+
+const cat_Woman = {
+    species: 'cat-human',
+    name: 'Liza',
+    gender: 'female',
+    hands: 2,
+    legs: 2,
+    saying: cat.saying,
+    friends: ['Dina', 'Vova', 'Liza'],
+};
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const inhabitants = [dog, cat, woman, man, cat_Woman];
+const properties = [
+    'species',
+    'name',
+    'gender',
+    'hands',
+    'legs',
+    'saying',
+    'friends',
+];
+inhabitants.forEach((obj) =>
+    print(
+        `<i>${properties
+            .map((prop) =>
+                !Array.isArray(obj[prop]) ? obj[prop] : obj[prop].join(', ')
+            )
+            .join('; ')}</i>`
+    )
+);
